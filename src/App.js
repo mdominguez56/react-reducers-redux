5f3eee3b7ae663b5f9a7e89d716759d4d30ff185
Matias Dominguez Alonso <matiasdominguez3@gmail.com>
2022-05-01 11:18:00 -0300

    new folder structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import Contador from "./components/Contador";
+import ContadorMejorado from "./components/ContadorMejorado";
 function App() {
   return (
     <div>
+      <hr />
       <h1>useReducer</h1>
       <Contador />
+      <hr />
+      <ContadorMejorado />
     </div>
   );
 }
--- src/components/Contador.jsx
+++ src/components/Contador.jsx
@@ -2,33 +2,54 @@ import React, { useReducer } from "react";
 
 const initialState = { contador: 0 };
 
+const init = (initialState) => {
+  return {
+    contador: initialState.contador + 100,
+  };
+};
+
 const TYPES = {
   INCREMENT: "INCREMENT",
   DECREMENT: "DECREMENT",
+  INCREMENT_5: "INCREMENT_5",
+  DECREMENT_5: "DECREMENT_5",
+  RESET: "RESET",
 };
 
 function reducer(state, action) {
   switch (action.type) {
     case TYPES.INCREMENT:
       return { contador: state.contador + action.payload };
+    case TYPES.INCREMENT_5:
+      return { contador: state.contador + action.payload };
     case TYPES.DECREMENT:
       return { contador: state.contador - action.payload };
+    case TYPES.DECREMENT_5:
+      return { contador: state.contador - action.payload };
+    case TYPES.RESET:
+      return initialState;
     default:
       return state;
   }
 }
 
 const Contador = () => {
-  const [state, dispatch] = useReducer(reducer, initialState);
+  const [state, dispatch] = useReducer(reducer, initialState, init);
 
   const sumar = () => dispatch({ type: TYPES.INCREMENT, payload: 1 });
+  const sumar5 = () => dispatch({ type: TYPES.INCREMENT, payload: 5 });
   const restar = () => dispatch({ type: TYPES.DECREMENT, payload: 1 });
+  const restar5 = () => dispatch({ type: TYPES.DECREMENT, payload: 5 });
+  const reset = () => dispatch({ type: TYPES.RESET });
   return (
     <div style={{ textAlign: "center" }}>
       <h2>Contador Reducer</h2>
       <nav>
         <button onClick={sumar}>+</button>
+        <button onClick={sumar5}>+5</button>
         <button onClick={restar}>-</button>
+        <button onClick={restar5}>-5</button>
+        <button onClick={reset}>0</button>
       </nav>
       <h3>{state.contador}</h3>
     </div>
